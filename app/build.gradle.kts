04c0cb6a2a70f74461dfe2b68800c1d306ac3421
PedroVilaNova <phpvn@discente.ifpe.edu.br>
2024-12-02 20:08:06 -0300

    Prática 01 - Parte 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.navigation.compose)
+    implementation("androidx.navigation:navigation-compose:2.8.4")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
